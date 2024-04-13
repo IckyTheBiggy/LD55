9f89158f3c16b36210c0493e62d4c4772810c55c
IckyTheBiggy <elcia116eb@gmail.com>
2024-04-14 00:20:17 +0100

    Improved the spawning system and fixed canvas on base health bar being big

--- Assets/Scripts/World/EnemySpawner.cs
+++ Assets/Scripts/World/EnemySpawner.cs
@@ -10,8 +10,6 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private List<GameObject> _enemyPrefabs;
     [SerializeField] private List<Transform> _enemySpawnPoints;
     [SerializeField] private int _enemiesPerWave;
-
-    private int[] _enemyTypesPerWave = { 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
     
     private int _currentWave;
     
@@ -26,20 +24,11 @@ public class EnemySpawner : MonoBehaviour
         {
             yield return new WaitForSecondsRealtime(_timeBetweenWaves);
 
-            List<int> availableEnemyTypes = new();
-            for (int i = 0; i < availableEnemyTypes[_currentWave]; i++)
-            {
-                availableEnemyTypes.Add(i);
-            }
-
             int enemiesToSpawn = Mathf.Min(_enemiesPerWave + _currentWave, 50);
 
             for (int i = 0; i < _enemiesPerWave; i++)
             {
-                int randomIndex = Random.Range(0, availableEnemyTypes.Count);
-                int enemyTypeIndex = availableEnemyTypes[randomIndex];
-
-                GameObject enemyPrefab = _enemyPrefabs[enemyTypeIndex];
+                GameObject enemyPrefab = _enemyPrefabs[Random.Range(0, _enemyPrefabs.Count)];
                 Transform spawnPoint = _enemySpawnPoints[Random.Range(0, _enemySpawnPoints.Count)];
                 
                 Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
